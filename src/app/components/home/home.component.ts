b13a118ef991c07dea9a13372766a0a6b196730d
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-11-22 16:48:33 -0300

    feat: Criando o componente projects

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { SkillsComponent } from '../skills/skills.component';
+import { ProjectsComponent } from '../projects/projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, AboutMeComponent, SkillsComponent],
+  imports: [HeaderComponent, AboutMeComponent, SkillsComponent, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
